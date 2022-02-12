2acc5efbb631d04556d0f7986f016eed31f228f6
Fer Becker <88803764+Ferordenana@users.noreply.github.com>
2022-02-12 17:27:18 -0300

    Update lol_randm_selector.py

--- lol_randm_selector.py
+++ lol_randm_selector.py
@@ -1,11 +1,11 @@
 import random
-
+#This program can create a list of random champs, you can modify the quantity of champs through the range("x"). Have fun :D
 
 champions_list = ["Aatrox", "Ahri", "Akali", "Akshan", "Allistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", "Aurelion Sol", "Azir", "Bardo", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopela", "Cho'gath", "corki", "Darius", "Diana", "Dr Mundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio","Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Hecarim" ,"Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna", "Jarvan IV", "Jax","Jayce", "Jhin", "Jinx", "Kai’Sa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Kha’Zix", "Kindred", "Kled", "Kog’Maw", "LeBlanc", "Lee Sin", "Leona", "Lillia", "Lissandra", "Lucian", "Lulu", "Lux", "Maestro Yi", "Malphite", "Malzahar", "Maokai", "Miss Fortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Neeko", "Nidalee", "Nocturne", "Nunu y Willump", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "Rek’Sai", "Rell", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Samira", "Sejuani", "Senna", "Seraphine", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Sylas", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel’Koz", "Vex", "Vi", "Viego", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo" ,"Yone", "Yorick", "Yuumi", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra" ]
 
 # random.choice to add random strings 
 # .append to add elements at the end of the array.
-# take out of the list el champ elegido .split() or .remove()
+# take out of the list the chosen champ .split() or .remove()
 
 
 
